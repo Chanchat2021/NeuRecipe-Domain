17818ad65a57da53bf9cd3df7e1e957d81fff00a
Chanchat2021 <91839104+Chanchat2021@users.noreply.github.com>
2022-10-10 15:59:18 +0530

    Add files via upload

---
+++ NeuRecipe.Domain/Entity/Recipe.cs
@@ -0,0 +1,19 @@
+﻿namespace NeuRecipe.Domain.Entity
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Ingredients { get; set; }
+        public string Directions { get; set; }
+        public string RecipeTips { get; set; }
+        public byte[]? Image { get; set; }
+        public string NutritionFacts { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? LastUpdatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public virtual User User { get; set; }
+        
+    }
+}
